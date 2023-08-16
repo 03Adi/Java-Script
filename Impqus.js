714a132769af8989264f28bc4c2a98873c6dc6f9
Aditya Prakash Pandey <adityapandey03012003@gmail.com>
2023-08-16 14:41:29 +0530

    Create Impqus.js

---
+++ Impqus.js
@@ -0,0 +1,13 @@
+//Simple Intrest
+let P=3,T=5,R=6
+let SI=(P*T*R)/100
+console.log(SI)
+
+//area of cicule
+let pi=3.14
+function findarea(r){
+    return(pi*r*r)
+}
+let r=5
+area=findarea(r)
+console.log(area)
