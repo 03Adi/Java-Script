9eec586d11d104cba5b7215189267005709c92c6
Aditya Prakash Pandey <adityapandey03012003@gmail.com>
2023-08-29 17:31:19 +0530

    Update oddevennum.js

--- oddevennum.js
+++ oddevennum.js
@@ -1,5 +1,15 @@
+//1st method-------
+
 var num=Number(prompt("enter the  number :")
 if(n%2==0)
   document.write(`${num} is even`)
 else
 document.write(`${num} is odd`)
+
+//2nd method--------
+
+var num=Number(prompt("enter the number"))
+if(num&1)
+  document.write(`${num} is odd`)
+else
+document.write(`${num} is even `)
